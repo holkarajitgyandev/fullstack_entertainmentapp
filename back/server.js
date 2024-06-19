59db664defff2c30a91887fcb5ff46312c529aad
holkarajitgyandev <139675885+holkarajitgyandev@users.noreply.github.com>
2024-06-19 12:53:55 +0530

    initial cimmit

---
+++ back/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors({
+orogin:["https://fullstack-moviesapp-api.vercel.app/"],
+methods:["POST","GET"],
+credentials:true
+}));
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://holkarajitgyandev:Hgfe9dcba@cluster0.wwtpzs9.mongodb.net/netflix?retryWrites=true&w=majority&appName=Cluster", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+app.get("/",(req,res)=>{res.json("hello")})
+app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
